1f0c5313780d35e5eb861e9fd4065fc1a11e5c36
tom juntunen <tom@kimono.com>
2025-02-17 19:49:03 -0800

    update start.sh fpr api

--- start.sh
+++ start.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# Start the Python API in the background
-python src/api/main.py &
-# Wait for the API to initialize
-sleep 5
-# Start the Streamlit app using the Heroku-provided port
-streamlit run src/app/main.py --server.port=$PORT
+# Start the Python API
+python src/api/main.py
